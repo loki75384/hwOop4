e05c173828785adffbc2cf068a8d3345d407aa0c
loki47 <larsentuta49@gmail.com>
2024-02-04 22:55:14 +0300

    add classwork

---
+++ src/main/java/Box.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Box <T extends Fruit>{
+
+    private ArrayList<T> arrayList;
+
+    public Box(ArrayList<T> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+            }
+
