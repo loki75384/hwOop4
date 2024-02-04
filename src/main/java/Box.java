bf363f0a540f2a71a3e36d759c28fcad76f47f9a
loki47 <larsentuta49@gmail.com>
2024-02-04 23:10:05 +0300

    добавил метод добавления фруктов в корзину,взвешивания т сравнивания с другими корзинами. Добавили условие при котором в одной корзине лежит 1 вид фруктов.

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -1,12 +1,33 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class Box <T extends Fruit>{
+class Box<T extends Fruit> {
+    private List<T> fruits = new ArrayList<>();
 
-    private ArrayList<T> arrayList;
-
-    public Box(ArrayList<T> arrayList) {
-        this.arrayList = arrayList;
+    public void addFruit(T fruit) {
+        fruits.add(fruit);
     }
 
-            }
+    public double getWeight() {
+        if (fruits.isEmpty()) {
+            return 0.0;
+        }
+        double weight = fruits.get(0) instanceof Apple ? 1.0 : 1.5;
+        return weight * fruits.size();
+    }
+
+    public boolean compare(Box<?> anotherBox) {
+        return Math.abs(this.getWeight() - anotherBox.getWeight()) < 0.0001;
+    }
+
+    public void transferFruitsToAnotherBox(Box<T> anotherBox) {
+        if (this.getClass() == anotherBox.getClass()) {
+            anotherBox.fruits.addAll(this.fruits);
+            this.fruits.clear();
+        } else {
+            System.out.println("Can't transfer fruits to a box of a different type");
+        }
+    }
+}
+
 
